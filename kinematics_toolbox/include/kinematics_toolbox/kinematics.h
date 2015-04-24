f40e7d8ed00433b631e15937dff5a15d34a99722
Dale McConachie <mcconachie@cmu.edu>
2015-04-24 18:36:09 -0400

    Partial rewrite of kinematics to use different format of input and output

--- kinematics_toolbox/include/kinematics_toolbox/kinematics.h
+++ kinematics_toolbox/include/kinematics_toolbox/kinematics.h
@@ -11,17 +11,18 @@ typedef Eigen::Matrix<double, 6, 1> Vector6d;
 typedef Eigen::Matrix<double, 6, 6> Matrix6d;
 typedef Eigen::Matrix<double, 6, Eigen::Dynamic> Matrix6Xd;
 
-Eigen::Matrix3f rot_z(double angle);
+Eigen::Matrix3d skew(const Eigen::Vector3d &w);
+Eigen::Vector3d unskew(const Eigen::Matrix3d &w_hat);
 
-void createTwist(Vector6d *xi, Eigen::Vector3d *omega, Eigen::Vector3d *q);
-void skew(Eigen::Matrix3d *w_hat, Eigen::Vector3d *w);
-void unskew(Eigen::Vector3d *w, Eigen::Matrix3d *w_hat);
-void twist_hat(Eigen::Matrix4d *s_hat, Vector6d *s);
-void twist_unhat(Vector6d *s, Eigen::Matrix4d *s_hat);
-void adj(Matrix6d *adj_G, Eigen::Matrix4d *G);
-void expmExact(Eigen::Matrix3d *expM, Eigen::Matrix3d *w_hat, double theta);
-void expTwist(Eigen::Matrix4d *expT, Vector6d *twist, double theta);
-void bodyJacobian(std::vector<Vector6d> *twists, std::vector<double> *theta, Eigen::Matrix4d *g_theta);
+Vector6d createTwist(const Eigen::Vector3d &omega, const Eigen::Vector3d &q);
+Eigen::Matrix4d twistHat(const Vector6d &xi);
+Vector6d twistUnhat(const Eigen::Matrix4d &xi_hat);
+
+Matrix6d adj(const Eigen::Matrix4d &g);
+Eigen::Matrix3d expmExact(const Eigen::Matrix3d &w_hat, const double theta);
+Eigen::matrix4d expTwist(const Vector6d &twist, const double theta);
+
+Matrix6Xd bodyJacobian(const std::vector<Vector6d> &twists, const std::vector<double> &theta, const Eigen::Matrix4d &g_theta);
 
 };
 
--- kinematics_toolbox/src/kinematics.cpp
+++ kinematics_toolbox/src/kinematics.cpp
@@ -1,64 +1,117 @@
 #include "kinematics_toolbox/kinematics.h"
 
 #include <math.h>
-#include <vector>
 
 using namespace kinematics;
 
-Eigen::Matrix3f rot_z(double angle)
+////////////////////////////////////////////////////////////////////////////////
+// Skew and Unskew for 3x3 matrices
+////////////////////////////////////////////////////////////////////////////////
+
+Eigen::Matrix3d skew(const Eigen::Vector3d w)
 {
-	Eigen::Matrix3f R;
+    Eigen::Matrix3d w_hat;
 
-	R << cos(angle), -sin(angle), 0,
-		 sin(angle),  cos(angle), 0,
-		 0,           0,          1;
-
-	return R;
+    w_hat <<     0, -w(2),  w(1),
+              w(2),     0, -w(0),
+             -w(1),  w(0),     0;
+    
+    return w_hat;
 }
 
-void bodyJacobian(std::vector<Vector6d> *twists, std::vector<double> *theta, Eigen::Matrix4d *g_theta) {
+Eigen::Vector3d unskew(const Eigen::Matrix3d &w_hat)
+{
+    Eigen::Matrix3d w;
+    Eigen::Matrix3d w_hat_sym;
+    w_hat_sym = ((*w_hat) - (*w_hat).transpose())/2;
     
-    int numTheta = theta->size();
-    Matrix6Xd J_s(6,numTheta);
-    Matrix6Xd J_b(6,numTheta);
-    Eigen::Matrix4d g = Eigen::Matrix4d::Identity();
-    Vector6d lastTwist;
-    Vector6d currentTwist;
-    Eigen::Matrix4d expT;
-    Matrix6d adjG;
-    
-    for(int i = 0; i < numTheta; i++) {
-        if (i == 0) {
-            currentTwist = (*twists)[i];
-            J_s.block<6,1>(0,i) = currentTwist;
-        }
-        else {
-            lastTwist = currentTwist;
-            currentTwist = (*twists)[i];
-            expTwist(&expT, &lastTwist, (*theta)[i-1]);
-            g = g * expT;
-            adj(&adjG, &g);
-            J_s.block<6,1>(0,i) = adjG*currentTwist;
-        }
+    w << w_hat_sym(2,1), w_hat_sym(0,2), w_hat_sym(1,0);
+
+    return w;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Twist create, twist hat and unhat
+////////////////////////////////////////////////////////////////////////////////
+
+Vector6d createTwist(const Eigen::Vector3d &omega, const Eigen::Vector3d &q)
+{
+    Vector6d xi;
+
+    if (omega(0) == 0 && omega(1) == 0 && omega(2) == 0)
+    {
+        xi.segment<3>(0) = q
+        xi.segment<3>(3) = omega;
     }
-    
-    adj(&adjG, g_theta);
-    J_b = adjG.inverse() * J_s;
-    
+    else
+    {
+        xi.segment<3>(0) = -1*omega.cross(q);
+        xi.segment<3>(3) = omega;
+    }
 }
 
-void expTwist(Eigen::Matrix4d *expT, Vector6d *twist, double theta) {
+Eigen::Matrix4d twistHat(const Vector6d &xi)
+{
+    Eigen::Matrix4d xi_hat = Eigen::Matrix4d::Zero();
+
+    Eigen::Vector3d v = s.segment<3>(0);
+    Eigen::Vector3d w = s.segment<3>(3);
+    Eigen::Matrix3d w_hat = skew(w)
     
+    xi_hat.block<3,3>(0,0) = w_hat;
+    xi_hat.block<3,1>(0,3) = v;
+}
+
+Vector6d twistUnhat(const Eigen::Matrix4d &xi_hat)
+{
+    Vector6d xi;
+
+    Eigen::Vector3d v = xi_hat.block<3,1>(0,3);
+    Eigen::Matrix3d w_hat = xi_hat.block<3,3>(0,0);
+    Eigen::Vector3d w = unskew(w_hat);
+    
+    xi.segment<3>(0) = v;
+    xi.segment<3>(3) = w;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Adjoints and twist exponentials
+////////////////////////////////////////////////////////////////////////////////
+
+Matrix6d adj(const Eigen::Matrix4d &g)
+{
+    Eigen::Matrix3d R = (*G).block<3,3>(0,0);
+    Eigen::Vector3d p = (*G).block<3,1>(0,3);
+    Eigen::Matrix3d p_hat;
+    skew(&p_hat, &p);
+    
+    (*adj_G).block<3,3>(0,0) = R;
+    (*adj_G).block<3,3>(0,3) = p_hat*R;
+    (*adj_G).block<3,3>(3,0) = Eigen::Matrix3d::Zero(3,3);
+    (*adj_G).block<3,3>(3,3) = R;
+}
+
+Eigen::Matrix3d expmExact(const Eigen::Matrix3d &w_hat, const double theta)
+{
+    Eigen::Matrix3d eye3 = Eigen::Matrix3d::Identity();
+    (*expM) = eye3 + (*w_hat) * sin(theta) + (*w_hat)*(*w_hat)*(1-cos(theta));
+}
+
+Eigen::matrix4d expTwist(const Vector6d &twist, const double theta)
+{
     Eigen::Vector3d v = (*twist).segment<3>(0);
     Eigen::Vector3d w = (*twist).segment<3>(3);
     
     (*expT) = Eigen::Matrix4d::Zero();
     
     
-    if (w(0)==0 && w(1)==0 && w(2)==0) {
+    if (w(0)==0 && w(1)==0 && w(2)==0)
+    {
         (*expT).block<3,1>(0,3) = v;
     }
-    else {
+    else
+    {
         Eigen::Matrix3d w_hat;
         skew(&w_hat, &w);
         
@@ -71,80 +124,43 @@ void expTwist(Eigen::Matrix4d *expT, Vector6d *twist, double theta) {
         (*expT).block<3,1>(0,3) = (eye3 - exp_w_hat_theta) * w.cross(v) + w*w.transpose()*v*theta;
         (*expT)(3,3) = 1;
     }
-    
 }
 
-void expmExact(Eigen::Matrix3d *expM, Eigen::Matrix3d *w_hat, double theta) {
-    
-    Eigen::Matrix3d eye3 = Eigen::Matrix3d::Identity();
-    (*expM) = eye3 + (*w_hat) * sin(theta) + (*w_hat)*(*w_hat)*(1-cos(theta));
-}
+////////////////////////////////////////////////////////////////////////////////
+// Geometric Jacobians
+////////////////////////////////////////////////////////////////////////////////
 
-void adj(Matrix6d *adj_G, Eigen::Matrix4d *G) {
+Matrix6Xd bodyJacobian(const std::vector<Vector6d> &twists, const std::vector<double> &theta, const Eigen::Matrix4d &g_theta)
+{
+    int numTheta = theta->size();
+    Matrix6Xd J_s(6,numTheta);
+    Matrix6Xd J_b(6,numTheta);
     
-    Eigen::Matrix3d R = (*G).block<3,3>(0,0);
-    Eigen::Vector3d p = (*G).block<3,1>(0,3);
-    Eigen::Matrix3d p_hat;
-    skew(&p_hat, &p);
-    
-    (*adj_G).block<3,3>(0,0) = R;
-    (*adj_G).block<3,3>(0,3) = p_hat*R;
-    (*adj_G).block<3,3>(3,0) = Eigen::Matrix3d::Zero(3,3);
-    (*adj_G).block<3,3>(3,3) = R;
-    
-}
+    Eigen::Matrix4d g = Eigen::Matrix4d::Identity();
 
-void twist_hat(Eigen::Matrix4d *s_hat, Vector6d *s) {
+    Vector6d lastTwist;
+    Vector6d currentTwist;
+    Eigen::Matrix4d expT;
+    Matrix6d adjG;
     
-    Eigen::Vector3d v = (*s).segment<3>(0);
-    Eigen::Vector3d w = (*s).segment<3>(3);
-    Eigen::Matrix3d w_hat;
-    skew(&w_hat, &w);
-    
-    (*s_hat) = Eigen::Matrix4d::Zero(4,4);
-    (*s_hat).block<3,3>(0,0) = w_hat;
-    (*s_hat).block<3,1>(0,3) = v;
-    
-}
-
-void twist_unhat(Vector6d *s, Eigen::Matrix4d *s_hat) {
-    
-    Eigen::Vector3d v = (*s_hat).block<3,1>(0,3);
-    Eigen::Matrix3d w_hat = (*s_hat).block<3,3>(0,0);
-    Eigen::Vector3d w;
-    unskew(&w, &w_hat);
-    
-    (*s).segment<3>(0) = v;
-    (*s).segment<3>(3) = w;
-}
-
-void skew(Eigen::Matrix3d *w_hat, Eigen::Vector3d *w) {
-    
-    (*w_hat) <<   0,    -(*w)(2),   (*w)(1),
-               (*w)(2),     0,     -(*w)(0),
-              -(*w)(1),  (*w)(0),       0;
-    
-}
-
-void unskew(Eigen::Vector3d *w, Eigen::Matrix3d *w_hat) {
-    
-    Eigen::Matrix3d w_hat_sym;
-    w_hat_sym = ((*w_hat) - (*w_hat).transpose())/2;
-    (*w) << w_hat_sym(2,1), w_hat_sym(0,2), w_hat_sym(1,0);
-}
-
-void createTwist(Vector6d *xi, Eigen::Vector3d *omega, Eigen::Vector3d *q) {
-    
-    if ((*omega)(0) == 0 && (*omega)(1) == 0 && (*omega)(2) == 0) {
-        (*xi).segment<3>(0) = (*q);
-        (*xi).segment<3>(3) = (*omega);
-    }
-    else {
-        (*xi).segment<3>(0) = -1*(*omega).cross(*q);
-        (*xi).segment<3>(3) = (*omega);
+    for(int i = 0; i < numTheta; i++)
+    {
+        if (i == 0)
+        {
+            currentTwist = twists[i];
+            J_s.block<6,1>(0,i) = currentTwist;
+        }
+        else
+        {
+            lastTwist = currentTwist;
+            currentTwist = twists[i];
+            expT = expTwist(lastTwist, theta[i-1]);
+            g = g * expT;
+            J_s.block<6,1>(0,i) = adj(g).inverse()*currentTwist;
+        }
     }
     
+    J_b = adj(g_theta).inverse() * J_s;
+
+    return J_b;
 }
-
-
-
