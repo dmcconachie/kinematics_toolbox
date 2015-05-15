34cfd0c711d6d909f228fe2209bda46f82bc9f37
Dale McConachie <mcconachie@cmu.edu>
2015-05-15 17:02:54 -0400

    modsnake nodes converted to PubSub inheritance.

--- kinematics_toolbox/include/kinematics_toolbox/kinematics.h
+++ kinematics_toolbox/include/kinematics_toolbox/kinematics.h
@@ -7,91 +7,91 @@
 namespace kinematics
 {
 
-typedef Eigen::Matrix< double, 6, 1 > Vector6d;
-typedef Eigen::Matrix< double, 7, 1 > Vector7d;
-typedef Eigen::Matrix< double, 6, 6 > Matrix6d;
-typedef Eigen::Matrix< double, 7, 7 > Matrix7d;
-typedef Eigen::Matrix< double, 6, Eigen::Dynamic > Matrix6Xd;
-typedef Eigen::Matrix< double, 7, Eigen::Dynamic > Matrix7Xd;
+  typedef Eigen::Matrix< double, 6, 1 > Vector6d;
+  typedef Eigen::Matrix< double, 7, 1 > Vector7d;
+  typedef Eigen::Matrix< double, 6, 6 > Matrix6d;
+  typedef Eigen::Matrix< double, 7, 7 > Matrix7d;
+  typedef Eigen::Matrix< double, 6, Eigen::Dynamic > Matrix6Xd;
+  typedef Eigen::Matrix< double, 7, Eigen::Dynamic > Matrix7Xd;
 
-typedef struct {
-  Vector6d vel;
-  bool valid;
-} Velocity6d;
+  typedef struct
+  {
+    Vector6d vel;
+    bool valid;
+  } Velocity6d;
 
-////////////////////////////////////////////////////////////////////////////////
-// Basic Transforms
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // Basic Transforms
+  ////////////////////////////////////////////////////////////////////////////////
 
-Eigen::Matrix4d rotX( const double theta );
-Eigen::Matrix4d rotY( const double theta );
-Eigen::Matrix4d rotZ( const double theta );
-Eigen::Matrix4d translate( const Eigen::Vector3d& p );
-Eigen::Matrix4d transX( const double x );
-Eigen::Matrix4d transY( const double y );
-Eigen::Matrix4d transZ( const double z );
+  Eigen::Matrix4d rotX( const double theta );
+  Eigen::Matrix4d rotY( const double theta );
+  Eigen::Matrix4d rotZ( const double theta );
+  Eigen::Matrix4d translate( const Eigen::Vector3d& p );
+  Eigen::Matrix4d transX( const double x );
+  Eigen::Matrix4d transY( const double y );
+  Eigen::Matrix4d transZ( const double z );
 
-////////////////////////////////////////////////////////////////////////////////
-// Skew and Unskew for 3x3 matrices
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // Skew and Unskew for 3x3 matrices
+  ////////////////////////////////////////////////////////////////////////////////
 
-Eigen::Matrix3d skew( const Eigen::Vector3d& w );
-Eigen::Vector3d unskew( const Eigen::Matrix3d& w_hat );
+  Eigen::Matrix3d skew( const Eigen::Vector3d& w );
+  Eigen::Vector3d unskew( const Eigen::Matrix3d& w_hat );
 
-////////////////////////////////////////////////////////////////////////////////
-// Twist create, cacluting twists, twist hat and unhat
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // Twist create, cacluting twists, twist hat and unhat
+  ////////////////////////////////////////////////////////////////////////////////
 
-Vector6d createTwist( const Eigen::Vector3d& omega, const Eigen::Vector3d& q );
-std::vector<Vector6d> createTwist( const std::vector< Eigen::Vector3d >& omega,
-                                   const std::vector< Eigen::Vector3d >& q );
-std::vector<Vector6d> calculateTwists( const Eigen::Matrix4d& g_base,
-                                       const std::vector< Eigen::Vector3d >& omega0,
-                                       const std::vector< Eigen::Vector3d >& q0 );
-Eigen::Matrix4d twistHat( const Vector6d& xi );
-Vector6d twistUnhat( const Eigen::Matrix4d& xi_hat );
+  Vector6d createTwist( const Eigen::Vector3d& omega, const Eigen::Vector3d& q );
+  std::vector<Vector6d> createTwist( const std::vector< Eigen::Vector3d >& omega,
+                                     const std::vector< Eigen::Vector3d >& q );
+  std::vector<Vector6d> calculateTwists( const Eigen::Matrix4d& g_base,
+                                         const std::vector< Eigen::Vector3d >& omega0,
+                                         const std::vector< Eigen::Vector3d >& q0 );
+  Eigen::Matrix4d twistHat( const Vector6d& xi );
+  Vector6d twistUnhat( const Eigen::Matrix4d& xi_hat );
 
-////////////////////////////////////////////////////////////////////////////////
-// Adjoints and twist exponentials
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // Adjoints and twist exponentials
+  ////////////////////////////////////////////////////////////////////////////////
 
-Matrix6d adj( const Eigen::Matrix4d& g );
-Eigen::Matrix3d expmExact( const Eigen::Matrix3d& w_hat, const double theta );
-Eigen::Matrix4d expTwist( const Vector6d& xi, double theta );
-Eigen::Matrix4d expTwist( const std::vector< Vector6d >& xi,
-                          const std::vector< double >& theta );
+  Matrix6d adj( const Eigen::Matrix4d& g );
+  Eigen::Matrix3d expmExact( const Eigen::Matrix3d& w_hat, const double theta );
+  Eigen::Matrix4d expTwist( const Vector6d& xi, double theta );
+  Eigen::Matrix4d expTwist( const std::vector< Vector6d >& xi,
+                            const std::vector< double >& theta );
 
-////////////////////////////////////////////////////////////////////////////////
-// Jacobians
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // Jacobians
+  ////////////////////////////////////////////////////////////////////////////////
 
-Matrix6Xd spatialJacobian( const std::vector< Vector6d >& xi,
-                           const std::vector< double >& theta );
-Matrix6Xd bodyJacobian( const std::vector< Vector6d >& xi,
-                        const std::vector< double >& theta,
-                        const Eigen::Matrix4d& g_zero );
+  Matrix6Xd spatialJacobian( const std::vector< Vector6d >& xi,
+                             const std::vector< double >& theta );
+  Matrix6Xd bodyJacobian( const std::vector< Vector6d >& xi,
+                          const std::vector< double >& theta,
+                          const Eigen::Matrix4d& g_zero );
 
-Eigen::Matrix< double, 1, Eigen::Dynamic > calculateJJointLimits(
-                            const std::vector< double >& theta,
-                            const double joint_limit );
+  Eigen::Matrix< double, 1, Eigen::Dynamic > calculateJJointLimits(
+                              const std::vector< double >& theta,
+                              const double joint_limit );
 
-Eigen::MatrixXd dampedPinv( const kinematics::Matrix7Xd& J,
-                            const std::vector< double >& theta,
-                            const double joint_limit,
-                            const double limit_threshold,
-                            const double manipubility_threshold,
-                            const double damping_ratio );
+  Eigen::MatrixXd dampedPinv( const kinematics::Matrix7Xd& J,
+                              const std::vector< double >& theta,
+                              const double joint_limit,
+                              const double limit_threshold,
+                              const double manipubility_threshold,
+                              const double damping_ratio );
 
-////////////////////////////////////////////////////////////////////////////////
-// Other
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // Other
+  ////////////////////////////////////////////////////////////////////////////////
 
-Vector6d calculateError( const Eigen::Matrix4d& g_current,
-                         const Eigen::Matrix4d& g_desired );
+  Vector6d calculateError( const Eigen::Matrix4d& g_current,
+                           const Eigen::Matrix4d& g_desired );
 
-double jointLimitError( const std::vector< double >& theta,
-                        const double joint_limit );
-
+  double jointLimitError( const std::vector< double >& theta,
+                          const double joint_limit );
 };
 
 #endif
