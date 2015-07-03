199fdbcd45813cec2a69db0fd2463d4b63521c00
Dale McConachie <mcconachie@cmu.edu>
2015-07-02 20:59:49 -0400

    Added some extra operators

--- kinematics_toolbox/src/kinematics_ros_types.cpp
+++ kinematics_toolbox/src/kinematics_ros_types.cpp
@@ -2,50 +2,18 @@
 
 using namespace kinematics_ros_types;
 
-geometry_msgs::Twist kinematics_ros_types::operator- (
-    geometry_msgs::Twist l, const geometry_msgs::Twist& r )
-{
-  l.linear.x -= r.linear.x;
-  l.linear.y -= r.linear.y;
-  l.linear.z -= r.linear.z;
-  l.angular.x -= r.angular.x;
-  l.angular.y -= r.angular.y;
-  l.angular.z -= r.angular.z;
+////////////////////////////////////////////////////////////////////////////////
+// Vector3 operators
+////////////////////////////////////////////////////////////////////////////////
 
-  return l;
-}
+////////////////////////////////////////////////////////////////////////////////
+// Twist operators
+////////////////////////////////////////////////////////////////////////////////
 
-geometry_msgs::Twist kinematics_ros_types::operator+ (
-    geometry_msgs::Twist l, const geometry_msgs::Twist& r )
-{
-  l.linear.x += r.linear.x;
-  l.linear.y += r.linear.y;
-  l.linear.z += r.linear.z;
-  l.angular.x += r.angular.x;
-  l.angular.y += r.angular.y;
-  l.angular.z += r.angular.z;
-
-  return l;
-}
-
-geometry_msgs::Twist kinematics_ros_types::zeroVelocity()
-{
-  geometry_msgs::Twist vel;
-  vel.linear.x = 0;
-  vel.linear.y = 0;
-  vel.linear.z = 0;
-  vel.angular.x = 0;
-  vel.angular.y = 0;
-  vel.angular.z = 0;
-  return vel;
-}
-
-/**
- * \brief Determines difference between two SE(2) poses that are in the same
- * plane with the same reference frame.
- *
- * \return A twist based representation of the difference in pose.
- */
+////////////////////////////////////////////////////////////////////////////////
+// Pose2D operators
+////////////////////////////////////////////////////////////////////////////////
+  
 geometry_msgs::Twist kinematics_ros_types::calculateError(
     const geometry_msgs::Pose2D& desired, const geometry_msgs::Pose2D& current )
 {
